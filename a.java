b0d1b795dcbd77ab150dfc452b139abf972ea6de
Shreya Chaudhari <114993484+Chaudharishreya@users.noreply.github.com>
2025-05-28 13:45:19 +0530

    Update a.java

--- a.java
+++ a.java
@@ -1,7 +1,16 @@
-// public class a{
-//     public static void main(String args[]){
-//         for (int i=0;i<=5;i++){
-//             for(int j=0;j<=5;j++){
+*****
+*****
+*****
+*****
+*****
+// public class a
+// {
+//     public static void main(String args[])
+//    {
+//         for (int i=0;i<5;i++)
+//         {
+//             for(int j=0;j<5;j++)
+//             {
 //                 System.out.print("*");
 //             }
 //             System.out.println();
@@ -10,19 +19,25 @@
 // }
 
 
-
-// public class a{
-//     public static void main(String args[]){
-//         for (int i=0;i<=5;i++){
-//             for(int j=0;j<=5;j++){
+  
+*****     *****
+*****     *****
+*****     *****
+*****     *****
+*****     *****
+// public class a
+// {
+//     public static void main(String args[])
+//   {
+//         for (int i=0;i<5;i++)
+//         {
+//             for(int j=0;j<5;j++){
 //                 System.out.print("*");
 //             }
-
-//             for(int j=0;j<=5;j++){
+//             for(int j=0;j<5;j++){
 //                 System.out.print(" ");
 //             }
-
-//             for(int j=0;j<=5;j++){
+//             for(int j=0;j<5;j++){
 //                 System.out.print("*");
 //             }
 //             System.out.println();
@@ -32,52 +47,28 @@
 
 
 
-
-
-// public class a {
-
-//   public static void main(String args[]) {
-//     for (int j = 1; j <= 5; j++) {
-//       for (int i = 1; i <= 5; i++) {
-//         System.out.print("*");
-//       }
-//       System.out.print("\t");
-//       for (int i = 1; i <= 5; i++) {
-//         System.out.print("*");
-//       }
-//       System.out.println();
-//     }
-//   }
+*****	*****
+*****	*****
+*****	*****
+*****	*****
+*****	*****
+// public class a 
+// {
+//     public static void main(String args[]) 
+//     {
+//         for (int j = 1; j <= 5; j++)
+//         {
+//            for (int i = 1; i <= 5; i++) {
+//            System.out.print("*");
+//            }
+//            System.out.print("\t");
+//            for (int i = 1; i <= 5; i++) {
+//            System.out.print("*");
+//            }
+//            System.out.println();
+//         }
+//    }
 // }
-
-
-// ***** *****
-// ***** *****
-// ***** *****
-// ***** *****
-// ***** *****
-
-
-
-
-
-// public class a {
-
-//     public static void main(String args[]) {
-//       for (int j = 1; j <= 5; j++) {
-//         for (int i = 1; i <= 5; i++) {
-//           System.out.print("*");
-//         }
-//         System.out.print("\t");
-//         for (int i = 1; i <= 5; i++) {
-//           System.out.print("*");
-//         }
-//         System.out.println();
-//       }
-//     }
-//   }
-
-
 
 
 // *****  *****    *****  *****  *****   
@@ -99,45 +90,33 @@
 // ***** *****    *****  *****  ***** 
 // ***** *****    *****  *****  ***** 
 
-
 // ***** *****    *****  *****  *****   
 // ***** *****    *****  *****  ***** 
 // ***** *****    *****  *****  ***** 
 // ***** *****    *****  *****  ***** 
 // ***** *****    *****  *****  ***** 
 
-
-
 // ***** *****    *****  *****  *****   
 // ***** *****    *****  *****  ***** 
 // ***** *****    *****  *****  ***** 
 // ***** *****    *****  *****  ***** 
 // ***** *****    *****  *****  ***** 
-
-
-
-
-
-
-
-
-  // public class a {
-
-  //   public static void main(String args[]) {
-
-  //   for(int l=0;l<=5;l++){
-  //       for(int k=1;k<=5;k++){
-  //           for(int j=1;j<=5;j++){
-  //               for(int i=1;i<=5;i++){
-  //                   System.out.print("*");
-  //               }
-  //               System.out.print(" ");
-               
-  //             }
-  //             System.out.println();
-  //       }
-  //       System.out.println();
-  //   }
-      
-  //   }
-  // }
+ public class a 
+ {
+    public static void main(String args[]) 
+   {
+       for(int l=0;l<=5;l++)
+       {
+          for(int k=1;k<=5;k++){
+            for(int j=1;j<=5;j++){
+              for(int i=1;i<=5;i++){
+                    System.out.print("*");
+              }
+              System.out.print(" ");
+              }
+              System.out.println();
+              }
+             System.out.println();
+       }
+    }
+  }
