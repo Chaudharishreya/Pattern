fed33f3297f7b884da5fd85dcf762a39411b9f6c
Shreya Chaudhari <114993484+Chaudharishreya@users.noreply.github.com>
2025-05-28 13:26:28 +0530

    Add files via upload

---
+++ b.java
@@ -0,0 +1,106 @@
+// public class b {
+//     public static void main(String args[]){
+    
+//         for(int i=1;i<=5;i++){
+//             for(int j=1;j<=5;j++){
+//                 System.out.print(i);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+// public class b {
+//     public static void main(String args[]){
+    
+//         for(int i=5;i>=1;i--){
+//             for(int j=1;j<=5;j++){
+//                 System.out.print(i);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// public class b {
+//     public static void main(String args[]){
+    
+//         for(int i=1;i<=5;i++){
+//             for(int j=1;j<=5;j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// public class b {
+//     public static void main(String args[]){
+    
+//         for(int i=1;i<=5;i++){
+//             for(int j=5;j>=1;j--){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// public class b {
+//     public static void main(String args[]){
+    
+//         for(int i=5;i>=1;i--){
+//             for(int j=1;j<=5;j++){
+//                 System.out.print((char)(64+i));
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// public class b {
+//     public static void main(String args[]){
+    
+//         for(int i=5;i>=1;i--){
+//             for(int j=1;j<=5;j++){
+//                 System.out.print((char)(64+i));
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+// public class b {
+//     public static void main(String args[]){
+    
+//         for(int i=5;i>=1;i--){
+//             for(int j=1;j<=5;j++){
+//                 System.out.print((char)(96+j));
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+public class b {
+    public static void main(String args[]){
+    
+        for(int i=5;i>=1;i--){
+            for(int j=5;j>=1;j--){
+                System.out.print((char)(96+j));
+            }
+            System.out.println();
+        }
+    }
+}
+
