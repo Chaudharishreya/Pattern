2886da54f1fd568861d8f1027fbb90921d77208f
Shreya Chaudhari <114993484+Chaudharishreya@users.noreply.github.com>
2025-05-28 13:26:00 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,143 @@
+// public class a{
+//     public static void main(String args[]){
+//         for (int i=0;i<=5;i++){
+//             for(int j=0;j<=5;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+// public class a{
+//     public static void main(String args[]){
+//         for (int i=0;i<=5;i++){
+//             for(int j=0;j<=5;j++){
+//                 System.out.print("*");
+//             }
+
+//             for(int j=0;j<=5;j++){
+//                 System.out.print(" ");
+//             }
+
+//             for(int j=0;j<=5;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+
+
+
+// public class a {
+
+//   public static void main(String args[]) {
+//     for (int j = 1; j <= 5; j++) {
+//       for (int i = 1; i <= 5; i++) {
+//         System.out.print("*");
+//       }
+//       System.out.print("\t");
+//       for (int i = 1; i <= 5; i++) {
+//         System.out.print("*");
+//       }
+//       System.out.println();
+//     }
+//   }
+// }
+
+
+// ***** *****
+// ***** *****
+// ***** *****
+// ***** *****
+// ***** *****
+
+
+
+
+
+// public class a {
+
+//     public static void main(String args[]) {
+//       for (int j = 1; j <= 5; j++) {
+//         for (int i = 1; i <= 5; i++) {
+//           System.out.print("*");
+//         }
+//         System.out.print("\t");
+//         for (int i = 1; i <= 5; i++) {
+//           System.out.print("*");
+//         }
+//         System.out.println();
+//       }
+//     }
+//   }
+
+
+
+
+// *****  *****    *****  *****  *****   
+// *****  *****    *****  *****  ***** 
+// *****  *****    *****  *****  ***** 
+// *****  *****    *****  *****  ***** 
+// *****  *****    *****  *****  ***** 
+
+
+// ***** *****    *****  *****  *****   
+// ***** *****    *****  *****  ***** 
+// ***** *****    *****  *****  ***** 
+// ***** *****    *****  *****  ***** 
+// ***** *****    *****  *****  ***** 
+
+// ***** *****    *****  *****  *****   
+// ***** *****    *****  *****  ***** 
+// ***** *****    *****  *****  ***** 
+// ***** *****    *****  *****  ***** 
+// ***** *****    *****  *****  ***** 
+
+
+// ***** *****    *****  *****  *****   
+// ***** *****    *****  *****  ***** 
+// ***** *****    *****  *****  ***** 
+// ***** *****    *****  *****  ***** 
+// ***** *****    *****  *****  ***** 
+
+
+
+// ***** *****    *****  *****  *****   
+// ***** *****    *****  *****  ***** 
+// ***** *****    *****  *****  ***** 
+// ***** *****    *****  *****  ***** 
+// ***** *****    *****  *****  ***** 
+
+
+
+
+
+
+
+
+  // public class a {
+
+  //   public static void main(String args[]) {
+
+  //   for(int l=0;l<=5;l++){
+  //       for(int k=1;k<=5;k++){
+  //           for(int j=1;j<=5;j++){
+  //               for(int i=1;i<=5;i++){
+  //                   System.out.print("*");
+  //               }
+  //               System.out.print(" ");
+               
+  //             }
+  //             System.out.println();
+  //       }
+  //       System.out.println();
+  //   }
+      
+  //   }
+  // }
