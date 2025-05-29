de8185cb4e4ce02009f5843920496e0ddc75045d
Shreya Chaudhari <114993484+Chaudharishreya@users.noreply.github.com>
2025-05-29 16:00:36 +0530

    d.java

---
+++ d.java
@@ -0,0 +1,79 @@
+// int a = 10;
+//  System.out.println(a++ - a-- - --a + ++a - a++ - a-- - ++a + a++ - a-- - --a); 
+Output :-- 43
+
+  
+// double a = 8;
+//  System.out.println(a);
+Output :-- 8.0
+
+  
+// System.out.print(152%28);
+Output :-- 12
+
+
+// int x = 100, y = 200;
+//    System.out.println("Before Swap");
+//    System.out.println("x =" + x);
+//    System.out.println("y =" + y);
+//    int temp = x;
+//    x = y;
+//    y = temp;
+//    System.out.println("After swap");
+//    System.out.println("x =" + x);
+//    System.out.println("y =" + y);
+Output :-- 
+  Before Swap
+        x =100
+        y =200
+After swap
+       x =200
+       y =100
+
+  
+ //  int i = 1;
+ //  int j = 10;
+ //  while(i<=10)
+ //  {
+ //    System.out.print( 5 + "*" + i + "=" + 5 * i+"\t");
+ //    i++;
+ //    System.out.print( 10 + "*" + j + "=" + 10 * j+"\t");
+ //    j--;
+ //    System.out.println();
+ // }
+Output :-- 
+ 5*1=5    10*10=100
+5*2=10   10*9=90
+5*3=15   10*8=80
+5*4=20   10*7=70
+5*5=25   10*6=60
+5*6=30   10*5=50
+5*7=35   10*4=40
+5*8=40   10*3=30
+5*9=45   10*2=20
+5*10=50 10*1=10
+
+
+// int i = 1;
+// while(i<=10)
+// {
+//     System.out.print( 5 + "*" + i + "=" + 5 * i+"\t");
+//     System.out.print( 10 + "*" + i + "=" + 10 * i+"\t");
+//     System.out.println();
+//     i++;
+// }
+Output :--
+5*1=5     10*1=10
+5*2=10   10*2=20
+5*3=15   10*3=30
+5*4=20   10*4=40
+5*5=25   10*5=50
+5*6=30   10*6=60
+5*7=35   10*7=70
+5*8=40   10*8=80
+5*9=45   10*9=90
+5*10=50 10*10=100
+  
+
+
+  
