ed5ca764013e41c8df63bdf7cbe592c215a1aa9c
Shreya Chaudhari <114993484+Chaudharishreya@users.noreply.github.com>
2025-05-29 19:23:27 +0530

    Create e.java

---
+++ e.java
@@ -0,0 +1,117 @@
+*
+**
+***
+****
+*****
+  
+// class e{
+//     public static void main(String args[])
+//     {
+//         int n=5;
+//         for(int i=1;i<=n;i++)
+//         {
+//           for(int j=i;j>=1;j--)
+//           {
+//               System.out.print("*");
+//           }
+//           System.out.println();
+//         }
+//     }
+// }
+
+
+  
+*****
+****
+***
+**
+*
+  
+// class e{
+//   public static void main(String args[])
+//   {
+//     int n=5;
+//     for(int i=1;i<=n;i++)
+//     {
+//       for(int j=i;j<=n;j++)
+//       {
+//          System.out.print((char)(i+96));
+//       }
+//       System.out.println();
+//     }
+//   }
+// } 
+
+
+  
+aaaaa
+bbbb
+ccc
+dd
+e  
+  
+// class e{
+//   public static void main(String args[])
+//   {
+//       int n=5;
+//       for(int i=1;i<=n;i++)
+//       {
+//         for(int j=i;j<=n;j++)
+//         {
+//           System.out.print((char)(i+96));
+//         }
+//         System.out.println();
+//       }
+//    }
+// }  
+
+
+1
+12
+123
+1234
+12345
+  
+// class e{
+//   public static void main(String args[])
+//   {
+//       int n=5;
+//       for(int i=1;i<=n;i++)
+//       {
+//           for(int j=1;j<=i;j++)
+//           {
+//               System.out.print(j);
+//           }
+//           System.out.println();
+//       }
+//   }
+// } 
+
+54321
+5432
+543
+54
+5
+
+// class e{                                            
+//   public static void main(String args[])            
+//   {                                                 
+//     int n = 5;
+//     for(int i=1;i<=n;i++)
+//       {
+//         for(int j=n;j>=i;i--)
+//           {
+//             System.out.println("*");
+//           }
+//         System.out.println();
+//       }
+//   }
+// }  
+  
+  
+  
+
+  
+  
+  
+    
