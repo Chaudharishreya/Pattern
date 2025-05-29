d08ceae95361c26939f58e71b4e3c478e59033ec
Shreya Chaudhari <114993484+Chaudharishreya@users.noreply.github.com>
2025-05-29 21:36:49 +0530

    Update e.java

--- e.java
+++ e.java
@@ -87,6 +87,8 @@ e
 //   }
 // } 
 
+
+  
 54321
 5432
 543
@@ -109,9 +111,267 @@ e
 // }  
   
   
+----*
+---**
+--***
+-****
+*****
   
+// class e{  
+// public static void main(String args[]) 
+//   {
+//    int n=5;
+//    for(int i=1;i<=n;i++)
+//    {
+//       for(int j=i;j<n;j++)
+//         {
+//             System.out.print("-");
+//         }
+//       for(int j=i;j>=1;j--)
+//         {
+//             System.out.print("*");
+//         }
+//             System.out.println();
+//     }
+//   }
+// }  
+  
+  
+*****
+-****
+--***
+---**
+----*
+  
+// class e{
+//   public static void main(String args[])
+//   {
+//      int n=5;
+//       for(int i=1;i<=n;i++)
+//       {
+//           for(int j=i;j>1;j--)
+//           {
+//             System.out.print("-");
+//           }
+//           for(int j=i;j<=n;j++)
+//           {
+//             System.out.print("*");
+//           }
+//           System.out.println();
+//         }
+//   }
+// }  
 
   
   
+    5
+   44
+  333
+ 2222
+11111
   
+// class e{
+//   public static void main(String args[])
+//   {
+//     int n=5;
+//     for(int i=5;i>=1;i--)
+//     {
+//       for(int j=i;j>1;j--)
+//       {
+//         System.out.print(" ");
+//       }
+//       for(int j=i;j<=n;j++)
+//       {
+//         System.out.print(i);
+//       }
+//       System.out.println();
+//     }
+//   }
+// }  
+
+
+ 11111
+  2222
+   333
+    44
+     5
+
+// class e{
+//   public static void main(String args[])
+//   {
+//       int n=5;
+//       for(int i=1;i<=n;i++)
+//       {
+//           for(int j=i;j>1;j--)
+//           {
+//               System.out.print(" ");
+//           }
+//           for(int j=i;j<=n;j++)
+//           {
+//               System.out.print(i);
+//           }
+//               System.out.println();
+//       }
+//   }
+
+     
+
+  54321
+   4321
+    321
+     21
+      1
+
+// class e{
+//   public static void main(String args[])
+//   { 
+//      int n=5;
+//      for(int i=n;i>=1;i--)
+//      {
+//         for(int j=i;j<n;j++)
+//         {
+//            System.out.print(" ");
+//         }
+//         for(int j=i;j>=1;j--)
+//         {
+//            System.out.print(j);
+//         }
+//         System.out.println();
+//      }
+//   }
+// }  
+
+
+*****
+****
+***
+**
+*
+  
+// class e{
+//   public static void main(String arg[])
+//   {
+//       int n=5;
+//       for(int i=1;i<=n;i++)
+//       {
+//         for(int j=n;j>=i;j--)
+//         {
+//            System.out.print("*");
+//         }
+//         System.out.println();
+//        }
+//   }
+// }  
+
+a
+ba
+cba
+dcba
+edcba
+
+// class e{
+//   public static vaid main(String args[])
+//   {
+//      int n=5;
+//      for(int i=1;i<=n;i++)
+//      {
+//         for(int j=i;j>n;j--)
+//         {
+//           System.out.print(" ");
+//         }
+//         for(int j=i;j>=1;j--)
+//         {
+//           System.out.print((char)(j+96));
+//         }
+//           System.out.println();
+//       }
+//   }
+// }  
+
+
+1
+21
+321
+4321
+54321
+
+// class e{
+//   public static void main(String args[])
+//   {
+//         int n=5;
+//         for(int i=1;i<=n;i++)
+//         {
+//             for(int j=i;j>=1;j--)
+//             {
+//                  System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//   }
+// }  
+
     
+5
+54
+543
+5432
+54321
+
+// class e{
+//   public static void main(String args[])
+//   {
+//     int n = 5;
+//     for(int i=n;i>=1;i--)
+//       {
+//         for(int j=n;j>=i;j--)
+//           {
+//             System.out.println(j);
+//           }
+//         System.out.println();
+//       } 
+//   }
+// } 
+
+
+5
+45
+345
+2345
+12345
+
+// class e{
+//   public static void main(String args[])
+//   {
+//     int n = 5;
+//     for(int i=n;i>=1;i--)
+//       {
+//         for(int j=i;i<=n;j++)
+//           {
+//             System.out.println(j);
+//           }
+//         System.out.println();
+//       }
+//   }
+// }  
+
+A
+BB
+CCC
+DDDD
+EEEEE
+
+class e{
+  public static void main(String args[])
+  {
+    int n = 5;
+    for(int i=1;i<=n;i++)
+    {
+      for(int j=1;j<=i;j++)
+        {
+          System.out.println(i);
+        }
+      System.out.println();
+    }
+  }
+}  
+        
